b49134be9a091150f8e05471da2f06921afcf2a5
kaz-under-the-bridge <kaz@under-the-bridge.work>
2024-01-15 02:16:14 +0900

    add organizations

---
+++ internal/domain/repository/organization/org_repository.go
@@ -0,0 +1,95 @@
+package organization
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/pkg/errors"
+
+	"github.com/kaz-under-the-bridge/mock-alert-notifier/internal/domain/model"
+	"github.com/kaz-under-the-bridge/mock-alert-notifier/internal/helper"
+	"github.com/kaz-under-the-bridge/mock-alert-notifier/internal/infrastracture/spreadsheet"
+)
+
+const (
+	spreadsheetId = "1fvHpjp1MgLDqPlo9WIfMhssuE9KUU05hCT3-Aln-D3Q"
+	readRange     = "OrganizationMaster!A3:G990" // ここを変更するときはvalidateRowDataのoffset値も変更すること
+)
+
+// ThreadUnsafeな挙動になるのでgoroutineなどで並列処理を行う場合は注意
+var sharedOrganizations *model.Organizations
+
+type RepositoryInterface interface {
+	GetOrganizations() (*model.Organizations, error)
+}
+
+type Repository struct {
+	ctx context.Context
+	ds  spreadsheet.SpreadsheetDatastoreInterface
+}
+
+func NewOrganizationRepository(ctx context.Context, ds spreadsheet.SpreadsheetDatastoreInterface) *Repository {
+	return &Repository{
+		ctx: ctx,
+		ds:  ds,
+	}
+}
+
+// データ型のvalidationのみ行う（それ以上のvalidationはmodel側で行う）
+func validateRowDataType(rowIndex int, data []interface{}) error {
+	rowOffset := 3 // readRangeの開始行が3行目なのでoffsetは3
+	colOffset := 0 // readRangeの開始列がA列なのでoffsetは0
+
+	col := helper.GetColSlice()
+
+	invalidDataError := errors.New("Invalid Data")
+
+	if _, ok := data[0].(int); !ok {
+		cell := fmt.Sprintf("%s%d", col[colOffset+0], rowIndex+rowOffset)
+		helper.Logger.Error("field should be Integer", slog.String("Cell", cell), slog.String("Value", fmt.Sprintf("%v", data)))
+		return errors.Wrapf(invalidDataError, "Cell: %s", cell)
+	}
+
+	for c, v := range data[1:5] {
+		if _, ok := v.(string); !ok {
+			cell := fmt.Sprintf("%s%d", col[colOffset+c+1], rowIndex+rowOffset)
+			helper.Logger.Error("filed should be String", slog.String("Cell", cell), slog.String("Value", fmt.Sprintf("%v", data)))
+			return errors.Wrapf(invalidDataError, "Cell: %s", cell)
+		}
+	}
+	return nil
+}
+
+func (r Repository) getOrganizationsFromSpreadsheet() (*model.Organizations, error) {
+	resp, err := r.ds.Values(r.ctx, spreadsheetId, readRange)
+	if err != nil {
+		helper.Logger.Error("spreadsheet service values got error", slog.String("SheetID", spreadsheetId), slog.String("ReadRange", readRange))
+		return nil, err
+	}
+
+	var orgs model.Organizations
+	for i, row := range resp.Values {
+		if err := validateRowDataType(i, row); err != nil {
+			return nil, err
+		}
+
+		org := model.NewOrganization(
+			row[0].(int),
+			row[1].(string),
+			row[2].(string),
+			row[3].(string),
+			row[4].(string),
+		)
+		orgs.Push(org)
+	}
+	sharedOrganizations = &orgs
+	return &orgs, nil
+}
+
+func (r Repository) GetOrganizations() (*model.Organizations, error) {
+	if sharedOrganizations != nil {
+		return sharedOrganizations, nil
+	}
+	return r.getOrganizationsFromSpreadsheet()
+}
---
+++ internal/domain/repository/organization/org_repository_test.go
@@ -0,0 +1,102 @@
+package organization
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kaz-under-the-bridge/mock-alert-notifier/internal/helper"
+	"github.com/kaz-under-the-bridge/mock-alert-notifier/internal/infrastracture/spreadsheet"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockOrganizationRepository struct{}
+
+func (m *MockOrganizationRepository) Values(ctx context.Context, spreasheetId, readRange string) (*spreadsheet.ValueResponse, error) {
+	return &spreadsheet.ValueResponse{
+		Range:          spreadsheetId,
+		MajorDimension: "test_major_dimension",
+		Values: [][]interface{}{
+			{1, "test_organization_name", "test_team_name", "test@example.com", "03-1234-5678"},
+		},
+	}, nil
+}
+
+func TestMain(m *testing.M) {
+	if err := helper.GetNewLogger(helper.SetLogType(context.Background(), "test")); err != nil {
+		panic(err)
+	}
+
+	m.Run()
+}
+
+func TestOrganizationRepository_GetOrganizations(t *testing.T) {
+	testCases := []struct {
+		id          int
+		name        string
+		team        string
+		email       string
+		phoneNumber string
+	}{
+		{
+			id:          1,
+			name:        "test_organization_name",
+			team:        "test_team_name",
+			email:       "test@example.com",
+			phoneNumber: "03-1234-5678",
+		},
+	}
+
+	ds := &MockOrganizationRepository{}
+	repo := NewOrganizationRepository(context.Background(), ds)
+	organizations, err := repo.GetOrganizations()
+
+	assert.NoError(t, err)
+
+	organization, ok := organizations.FindByID(1)
+	assert.Equal(t, true, ok)
+
+	for _, tc := range testCases {
+		assert.Equal(t, tc.id, organization.ID)
+		assert.Equal(t, tc.name, organization.Name)
+		assert.Equal(t, tc.team, organization.Team)
+		assert.Equal(t, tc.email, organization.Email)
+		assert.Equal(t, tc.phoneNumber, organization.PhoneNumber)
+	}
+}
+
+/*
+以下のような表形式のデータのvalidation
+1 A B        C       D                       E
+2 1 ◯株式会社 チーム太郎 team-taro@example.com   090-1234-5678 <- OK
+3 2 株式会社△ チーム花子 team-hanako@example.com 080-9876-5432 <- OK
+4 a test     test    a@b.c                   123-4567-8901 <- NG
+5 4 1        test    a@b.c                   123-4567-8901 <- NG
+6 5 test     2       a@b.c                   123-4567-8901 <- NG
+7 6 test     test    3                       123-4567-8901 <- NG
+8 7 test     test    a@b.c                   a             <- NG
+*/
+func TestValidateRowDataType(t *testing.T) {
+	testCases := []struct {
+		data        []interface{}
+		isError     bool
+		msgExpected string
+	}{
+		{data: []interface{}{1, "◯株式会社", "チーム太郎", "team-taro@example.com", "090-1234-5678"}, isError: false, msgExpected: ""},
+		{data: []interface{}{2, "株式会社△", "チーム花子", "team-hanako@example.com", "080-9876-5432"}, isError: false, msgExpected: ""},
+		{data: []interface{}{"a", "test", "test", "a@b.c", "123-4567-8901"}, isError: true, msgExpected: "Cell: A5: Invalid Data"},
+		{data: []interface{}{4, 1, "test", "a@b.c", "123-4567-8901"}, isError: true, msgExpected: "Cell: B6: Invalid Data"},
+		{data: []interface{}{5, "test", 2, "a@b.c", "123-4567-8901"}, isError: true, msgExpected: "Cell: C7: Invalid Data"},
+		{data: []interface{}{6, "test", "test", 3, "123-4567-8901"}, isError: true, msgExpected: "Cell: D8: Invalid Data"},
+		{data: []interface{}{7, "test", "test", "a@b.c", 1}, isError: true, msgExpected: "Cell: E9: Invalid Data"},
+	}
+
+	for r, tc := range testCases {
+		err := validateRowDataType(r, tc.data)
+		if tc.isError {
+			assert.Error(t, err)
+			assert.Equal(t, tc.msgExpected, err.Error())
+		} else {
+			assert.NoError(t, err)
+		}
+	}
+}
